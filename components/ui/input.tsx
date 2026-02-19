d6a0df526e1b6cc7c2febe4a0f3e6e51d9ac8309
adarsh39565-hue <adarsh39565@gmail.com>
2026-02-20 00:35:57 +0530

    Refactor Input component for improved styling

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,14 +1,16 @@
 import * as React from "react";
+import { cn } from "@/lib/utils";
 
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
-
-export function Input({ className = "", ...props }: InputProps) {
+export function Input({
+  className,
+  ...props
+}: React.InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
-      className={
-        "w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm text-gray-900 placeholder:text-gray-400 outline-none focus:ring-2 focus:ring-black " +
+      className={cn(
+        "flex h-10 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500",
         className
-      }
+      )}
       {...props}
     />
   );
