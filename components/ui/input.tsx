9b782d0830a799d7ea6ee0241caad7aa7b2d7fc7
adarsh39565-hue <adarsh39565@gmail.com>
2026-02-19 21:54:46 +0530

    Add input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,1 +1,17 @@
+import * as React from "react";
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+export function Input({ className = "", type, ...props }: InputProps) {
+  return (
+    <input
+      type={type}
+      className={
+        "flex h-10 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm text-gray-900 placeholder:text-gray-400 outline-none focus:ring-2 focus:ring-black disabled:cursor-not-allowed disabled:opacity-50 " +
+        className
+      }
+      {...props}
+    />
+  );
+}
 
